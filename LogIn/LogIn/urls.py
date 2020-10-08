66cfa17a149bcbfd3540d5c0826dc53f138ccaaf
Shiva Shrestha <64135683+sh7vashrestha@users.noreply.github.com>
2020-10-08 22:02:07 +0545

    Test test

--- LogIn/LogIn/urls.py
+++ LogIn/LogIn/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Id_password.urls'))
+    path('', include('Id_password.urls')),
+    path('', include('travel.urls'))
 ]
